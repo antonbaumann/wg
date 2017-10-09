214dbf4e743212f2039fb631e68cbe7f21882ab7
antonbaumann <antonbaumann4@gmail.com>
2017-10-09 22:29:02 +0200

    some improvements global debugging switch

--- 02_Schwimmbad/schwimmbad.py
+++ 02_Schwimmbad/schwimmbad.py
@@ -1,58 +1,120 @@
-def calculate_price(we, fe, g, k, j, e):
-    preise = {
-              "e":  3.5,    # Erwachsener   (Einzelkarte)
-              "j":  2.5,    # Jugendlischer (Einzelkarte)
-              "t": 11.0,    # Tageskarte
-              "f":  8.0     # Familienkarte
-              }
+import prices
+from prices import show_table
 
-    # Wenn Ferien -> Gutscheine ungültig
-    if fe:
-        g = 0
+global V
+V = False
 
-    # Am Wochenende werden keine Tageskarten verkauft
-    if we:
-        preise.pop("t")
 
-    # An Wochentagen gibt es -20% Rabatt auf Einzelkarten
-    else:
-        preise["e"] = 2.8
-        preise["j"] = 2.0
-
-    # Wenn Kinder unter vier Jahren dabei sind
-    if k > 0:
-        # Wenn kein Erwachsener dabei ist -> Abbruch
-        if e == 0:
-            print("Kinder unter 4 Jahren müssen vin mind. einem Erwachsenen begleitet werden!")
+def finished(lst):
+    for a, b in lst:
+        if a != b:
             return False
-        else:
-            k = 0
+    return True
 
-    print(g, (j, e), preise)
 
-    p_e = {e*preise['e'] + j*preise['j']: [(e, 'e'), (j, 'j')]}
-    print(p_e)
+def remove_duplicates(lst):
+    c = []
+    tmp = [0, 0]
+    for e in lst:
+        comp = [e[0], e[1]]
+        b = True
+        for i in range(len(tmp)):
+            if tmp[i] != comp[i]:
+                b = False
+        if not b:
+            c.append(e)
+        tmp = comp
+    return c
 
-    # Maximiere Anzahl an Tageskarten
-    if 't' in preise:
-        t_p = [e, j]
-        nr_t = (e + j) // 6
-        nr = nr_t * 6
 
-        while nr > 0:
-            if t_p[0] > 0:
-                t_p[0] -= 1
-            elif t_p[1] > 0:
-                t_p[1] -= 1
-            nr -= 1
+def main(we, fe, g, k, j, e):
+    if k > 0 and e == 0:
+        print("Kinder unter 4J dürfen nicht ohne begleitung eines Erwachsenen ins Schwimmbad gehen!")
+        return False
 
-        p_t = {nr_t*preise['t'] + t_p[0]*preise['e'] + t_p[1]*preise['j']: [(nr_t, 't'), (t_p[0], 'e'), (t_p[1], 'j')]}
-        print(p_t)
+    table = prices.prices(e, j, we, 0)
+    h = []
+    stack = []
+    poss = []
+    min_costs = 999999999999
 
-    # Maximiere Anzahl an Familienkarten
-    t_p = [e, j]
-    nr_f = 0
+    if len(table) > 2:
+        table = table[:-2]
+    stack.append([e, j, table, 0])
+    h.append([len(table), 0])
 
+    costs = 0
+    tmp = []
+    while not finished(h):
+        top = stack[len(stack) - 1]
+        t = top[2]
+
+        if V:
+            print(h)
+            print(top[0], top[1], top[3])
+            show_table(t)
+
+        row = t[h[len(h) - 1][1]]
+        tmp.append(row)
+
+        costs += row['c']
+        new = [
+            top[0] - row['e'],
+            top[1] - row['j'],
+            prices.prices(top[0] - row['e'], top[1] - row['j'], we, 0),
+            top[3] + 1
+        ]
+
+        h[len(h) - 1][1] += 1
+
+        if new[0] == new[1] == 0:
+            if V:
+                print(costs)
+                print(tmp)
+                print()
+                print("##################################################################")
+
+            poss.append([costs, sorted(tmp, key=lambda k: k['c'], reverse=True)])
+
+            if costs < min_costs:
+                min_costs = costs
+
+            p = tmp.pop()
+            costs -= p['c']
+            while len(h) != 0 and h[len(h) - 1][0] == h[len(h) - 1][1]:
+                stack.pop()
+                h.pop()
+                if len(tmp) > 0:
+                    p = tmp.pop()
+                    costs -= p['c']
+
+        if len(new[2]) != 0:
+            stack.append(new)
+            h.append([len(new[2]), 0])
+
+    poss = sorted(poss, key=lambda k: k[0])
+
+    return remove_duplicates(poss)
 
 if __name__ == '__main__':
-    calculate_price(we=False, fe=True, g=0, k=1, j=4, e=5)
+    we = True
+    fe = False
+    g = 1
+    k = 1
+    j = 7
+    e = 9
+
+    lst = main(we, fe, g, k, j, e)
+    if len(lst) > 7:
+        lst = lst[:7]
+    for p in lst:
+        print(p[0])
+        prices.show_table(p[1])
+
+        # while g > 0 and not fe:
+        #     for p in lst:
+        #         price = p[0]
+        #         cards = p[1]
+        #         p_group = price * 9 / 10
+        #         p_single = price
+        #         if e > 0:
